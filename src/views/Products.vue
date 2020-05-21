83ccf4d319fa997db2ff6afbdee2d064746aafc9
JHC <jhc060109@naver.com>
2020-05-22 01:50:32 +0900

    add : 상품 등록 기능 추가

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -13,14 +13,61 @@
                         </div>
                     </div>
                 </div>
+
+                <hr />
+
+                <h3>Basic CRUD (create/read/update/delete) in Firebase and Vue</h3>
+
+                <div class="product-test">
+                    <div class="form-group">
+                        <input
+                            type="text"
+                            placeholder="Product Name"
+                            v-model="product.name"
+                            class="form-control"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" placeholder="Price" v-model="product.price" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <button @click="saveData" class="btn btn-primary">Save Data</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { db } from "../firebase";
+
 export default {
     name: "Product",
+    data() {
+        return {
+            product: {
+                name: null,
+                price: null
+            }
+        };
+    },
+    methods: {
+        saveData() {
+            db.collection("products")
+                .add(this.product)
+                .then((docRef) => { // to access "this", you should use lamda.
+                    console.log("Document written with ID: ", docRef.id);
+                    this.resetData();
+                })
+                .catch(function(error) {
+                    console.error("Error adding document: ", error);
+                });
+        },
+        resetData() {
+            Object.assign(this.$data, this.$options.data.apply(this));
+        }
+    },
     props: {
         msg: String
     }
