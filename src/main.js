57f647758a7bbb3af7200423de07c433c56942ae
JHC <jhc060109@naver.com>
2020-05-28 00:53:51 +0900

    add : 알림 modal 플러그인 설치

--- src/main.js
+++ src/main.js
@@ -9,12 +9,35 @@ import VueFirestore from "vue-firestore";
 import "popper.js";
 import "bootstrap";
 import "bootstrap/scss/bootstrap.scss";
+import Swal from "sweetalert2";
 
 import NavBar from "./components/NavBar.vue";
 
 Vue.config.productionTip = false;
 Vue.component(NavBar.name, NavBar);
 Vue.use(VueFirestore);
+Vue.prototype.Swal = Swal;
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener(
+            "mouseenter",
+            Swal.stopTimer
+        );
+        toast.addEventListener(
+            "mouseleave",
+            Swal.resumeTimer
+        );
+    },
+});
+
+window.Toast = Toast;
+
 // Vue.component('NavBar', require('./components/NavBar.vue').default);
 
 new Vue({
